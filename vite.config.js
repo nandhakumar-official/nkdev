f611596e81c282647b52e925605643d603d13620
Nandha Kumar <nchinnasami@siddhaai.com>
2026-07-23 15:58:24 +0530

    Initial Portfolio

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    open: true,
+    port: 3007,
+  },
+  base: "/NKDEV/",
+});
